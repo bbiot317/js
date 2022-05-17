838df7768568189cb448b6aba1e94a4005ad8771
bbiot317 <gala52s5g@gmail.com>
2022-05-17 17:05:14 +0900

    m

--- temprun/Cinfo.py
+++
@@ -1,4 +0,0 @@
-class  info:
-    def __init__(self,name,email):
-        self.name = name
-        self.email = email
--- temprun/car.py
+++
@@ -1,7 +0,0 @@
-class car:
-    def __init__(self):
-        self.size = 16
-    def prtt(self):
-        print("ㅍㄾ")
-    def prtt():
-        print("ㅍㄾㅌ")
